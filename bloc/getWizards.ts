d0845dfa168e1b81ba09f2d862e886c4673907be
Ahmed Shaaban <111895280+AhmedShaban52@users.noreply.github.com>
2026-06-09 23:57:14 +0300

    replace color from global.css file

--- bloc/getWizards.ts
+++ bloc/getWizards.ts
@@ -2,9 +2,9 @@ import api from "@/lib/axios";
 import { Wizard } from "@/types/types";
 
 const WIZARD_COLORS = [
-  "py-3 px-2 bg-[#EE9800]/10 text-[#FFB95F] text-xs border border-[#EE9800]/20",
+  "py-3 px-2 bg-[#EE9800]/10 text-yellow text-xs border border-[#EE9800]/20",
   "py-1 px-3 bg-therty/10 text-therty border border-therty/20",
-  "py-1 px-3 bg-[#EE9800]/10 text-[#FFB95F] border border-[#EE9800]/20",
+  "py-1 px-3 bg-[#EE9800]/10 text-yellow border border-[#EE9800]/20",
 ];
 
 export const fetchWizards = async (): Promise<Wizard[]> => {
